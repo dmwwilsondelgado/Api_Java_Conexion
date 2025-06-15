6ca84558dfaa0f661fd21fae27882f18746243fe
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-15 15:20:53 -0700

    usario java

--- src/MODELO/Usuario.java
+++ src/MODELO/Usuario.java
@@ -8,6 +8,26 @@ package MODELO;
  *
  * @author wilso
  */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 public class Usuario {
-    
+    Conexion conexion = new Conexion();
+
+    public boolean login(String correo, String password) {
+        String sql = "SELECT * FROM usuarios WHERE correo = ? AND password = ?";
+        try (Connection con = conexion.conectar();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, correo);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            return rs.next(); // true si encuentra al usuario
+
+        } catch (Exception e) {
+            System.out.println("Error al verificar login: " + e.getMessage());
+            return false;
+        }
+    }
 }
