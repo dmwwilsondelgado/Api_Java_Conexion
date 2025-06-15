5c6e96bd940ef1dc6eb960fb45be0ccb1abd9eef
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-15 15:10:47 -0700

    modelo usuario

--- src/MODELO/Conexion.java
+++ src/MODELO/Conexion.java
@@ -3,15 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package MODELO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.io.*;
+import java.sql.SQLException;
 
 /**
  *
  * @author wilso
  */
 public class Conexion {
-    public PreparedStatement sql;
+    public PreparedStatement sql; // trabajo para las sentecias de sql 
+    public ResultSet datos;// traer datos de la bd
+    public Connection con=null; // conexion ala base de datos
 
-   public void MetodoConectar(){
-       
-   }
+    public final String url = "jdbc:mysql://localhost:3306/floresteria_encanto_database";
+    public final String user = "wilsondelgado";
+    public final String password = "1102717619";
+    public Connection conexion;
+    public Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión exitosa.");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+        return conexion;
+    }
 }
